eeed6e3bef48a0fa02b9f636300e255b9cce9c6c
Swaroop Kaliki <ratnaswaroopk@gmail.com>
2025-06-12 14:53:24 +0530

    Fix: return JSON from get-config endpoint

--- app.py
+++ app.py
@@ -2,18 +2,15 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/send-data', methods=['POST'])
-def receive_data():
-    data = request.get_json()
-    print("Received from Raspberry Pi:", data)
-    return "Data received!", 200
-
 @app.route('/get-config', methods=['GET'])
-def send_config():
-    config = {'sampling_interval': 10, 'mode': 'auto'}
-    return jsonify(config)
-
-if __name__ == '__main__':
-    import os
+def get_config():
+    config = {
+        "fan": "off",
+        "threshold": 30
+    }
+    return jsonify(config)  
+# make sure this is at the end
+import os
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
