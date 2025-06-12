adb0602cfd10b12109031039771618f48f9ba3ed
Swaroop Kaliki <ratnaswaroopk@gmail.com>
2025-06-12 16:50:50 +0530

    Add /send-data POST endpoint

--- app.py
+++ app.py
@@ -15,3 +15,8 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+@app.route('/send-data', methods=['POST'])
+def send_data():
+    data = request.get_json()
+    print("Received data:", data)  # For logging on Render
+    return jsonify({"message": "Data received", "data": data})
